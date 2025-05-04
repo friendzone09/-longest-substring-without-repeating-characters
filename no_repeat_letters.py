f7abedf03c55fe85cbdce92b6930bc8e5f9e0b66
Alejandro D Leon <alejandroperez9474@gmail.com>
2025-05-04 12:26:58 -0600

    Codigo corregido

--- no_repeat_letters.py
+++ no_repeat_letters.py
@@ -1,20 +1,19 @@
-text = input('Introduce un texto: ')
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
 
-def checktext(text):   
-    
-    letters = list(text)
-    newTextArray = []
-    newText = ''
-    
-    for letter in letters:
-        if letter not in newTextArray:
-            newTextArray.append(letter)
-    
-    for letter in newTextArray:
-        newText += letter
+        char_set = set()
+        left = 0
+        max_len = 0
         
-    print(len(newTextArray))
-    
-    print(f'The answer is "{newText}" with the length of {len(newTextArray)}')
-    
-checktext(text)
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left +1
+            char_set.add(s[right])
+            max_len = max(max_len, right - left + 1)
+        
+        return max_len
