65960fba3eb2bc2adef9c41b499336399743cb2b
Alejandro D Leon <alejandroperez9474@gmail.com>
2025-05-07 11:49:18 -0600

    Agregando left +=1

--- no_repeat_letters.py
+++ no_repeat_letters.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
         char_set = set()
         left = 0
         max_len = 0
@@ -12,7 +11,7 @@ class Solution(object):
         for right in range(len(s)):
             while s[right] in char_set:
                 char_set.remove(s[left])
-                left +1
+                left +=1
             char_set.add(s[right])
             max_len = max(max_len, right - left + 1)
         
